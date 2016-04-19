3cbca2ea8ca5aa12329eb60230806334234c6093
dPeluChe <olimpuz@gmail.com>
2016-04-19 16:47:23 -0500

    Primeras palabras del BOT

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+
+curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=<CAAOgpWyZBioEBABY1VvJbafMzOjRQ4YyB16ZAis16atljuMISrIplZCcV6xRFZAJNDwAF5IGGLaJuZA6CVvEeSYvvj7gtdXmQY7ctcDCjEb58SoB541uMqRiWvqqfWWSFI2ZC9ru7JPwWy1uInlLLvJS85ZAZB14lno6PzGgNMjWZCsXI96KAoeBzVeAAHF66ZC9kCVixsM15ffAZDZD>"
+
+CAAOgpWyZBioEBABY1VvJbafMzOjRQ4YyB16ZAis16atljuMISrIplZCcV6xRFZAJNDwAF5IGGLaJuZA6CVvEeSYvvj7gtdXmQY7ctcDCjEb58SoB541uMqRiWvqqfWWSFI2ZC9ru7JPwWy1uInlLLvJS85ZAZB14lno6PzGgNMjWZCsXI96KAoeBzVeAAHF66ZC9kCVixsM15ffAZDZD
+
+*/
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
@@ -14,15 +21,57 @@ app.use(bodyParser.json())
 // Index route
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
+    //res.send('1660821720')
 })
 
 // for Facebook verification
+/*
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'CAAOgpWyZBioEBAKLWSN1oLlvZCmyjE8oc8EsvWOy7VE9Oz4WtwmSQZBZC8G8VrZB1D0N0Oaie7HglY4Gul1wivkiZCaFtXcYMEcZClAXVPCrK78flITK1RblZBb3J7JSZCeojZCHRXZAhXAXW6TvMNZBKPcQgHMkIx1UE75dwqOuZAWTE7qXWsdIZCRpTRYDRTiOkIyFh06ni2ToHgcAZDZD') {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
+*/
+
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "<CAAOgpWyZBioEBABY1VvJbafMzOjRQ4YyB16ZAis16atljuMISrIplZCcV6xRFZAJNDwAF5IGGLaJuZA6CVvEeSYvvj7gtdXmQY7ctcDCjEb58SoB541uMqRiWvqqfWWSFI2ZC9ru7JPwWy1uInlLLvJS85ZAZB14lno6PzGgNMjWZCsXI96KAoeBzVeAAHF66ZC9kCVixsM15ffAZDZD>"
+
+
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
